8d30eb8096076ec032deba23d3c75cf4682b356c
Justin Triplett <justin.triplett@gmail.com>
2015-06-01 01:45:04 -0400

    add generateMC.py to repo and private.py to ignore

---
+++ generateMC.py
@@ -0,0 +1,79 @@
+from sys import argv
+
+import barcode
+import requests
+
+import private
+
+
+def svg(code_list, savepath):
+
+    print('SVG')
+    for code in code_list:
+        print('generating... ' + code + '.svg')
+
+        try:
+            output = barcode.Code39(code, add_checksum=False)
+            filename = output.save(savepath + code)
+        except:
+            print('Error: exception in generating svg')
+
+
+def emf(code_list, savepath):
+
+    # create svg files to be converted by Cloud Convert REST API
+    svg(code_list, savepath)
+
+    # attempt generating emf files
+    try:
+        print('EMF')
+        for code in code_list:
+            print('generating...' + code + '.emf')
+            request = requests.post(private.my_api_url,
+                                    files={'file': open(savepath + code + '.svg', 'rb')})
+            with open(savepath + code + '.emf', 'wb') as fd:
+                for chunk in request.iter_content(1024):
+                    fd.write(chunk)
+    except:
+        print('Error: exception in generating emf')
+
+
+
+def png(code_list, savepath):
+    print('PNG')
+    # TODO png support, will require PIL
+    pass
+
+
+def main():
+    '''
+    Take a list of strings and generate a Code39 barcode in different formats
+    :return: none, result is saved as files
+    '''
+    # list of strings to turn into barcodes
+    code_list = []
+
+    # where to save the output, also should be 'working' folder
+    savepath = 'C:\Users\jtripl01\PycharmProjects\Code39\\'
+
+    if len(argv) == 1:
+        # default codes
+        code_list = ['ABCDEF', 'qwe123', '33']
+        svg(code_list, savepath)
+
+    elif len(argv) > 1:
+        # skip script name and first argument which should be format
+        code_list = argv[2:]
+        file_format = argv[1]
+
+        # generate vector files according to selected file type
+        if file_format == 'svg':
+            svg(code_list, savepath)
+        elif file_format == 'emf':
+            emf(code_list, savepath)
+        elif file_format == 'png':
+            png(code_list, savepath)
+
+
+if __name__ == '__main__':
+    main()
